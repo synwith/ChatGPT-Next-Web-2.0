b982186c090638212b756c3c682b9545a4d50fc8
Nil Lu <synwith@gmail.com>
2023-05-10 16:32:44 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>涌现学园智师AI By智谷开放</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          打造您的私人领域AI系统，微信viyi88
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
